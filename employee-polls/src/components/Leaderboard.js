c9c29f26c2ba7417c3d8a3f3196be2bddd46fc8e
todd-estes <elestes@gmail.com>
2024-10-02 16:45:43 -0400

    feat: Implement Leaderboard functionality

--- employee-polls/src/components/Leaderboard.js
+++ employee-polls/src/components/Leaderboard.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
 
 const Leaderboard = () => {
+  const users = useSelector((state) => state.users)
+
+  const userTotals = Object.values(users).map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      avatarURL: user.avatarURL,
+      answeredCount: Object.values(user.answers).length,
+      createdCount: user.questions.length,
+      total: Object.keys(user.answers).length + user.questions.length,
+    };
+    }
+  )
+
+  const orderedUsers = userTotals.sort((a, b) => b.total - a.total);
+
   return (
     <div className="container">
       <div className="content">
@@ -8,12 +25,29 @@ const Leaderboard = () => {
         <table className="leaderboard-table">
           <thead>
             <tr>
-              <th>Rank</th>
-              <th>Name</th>
-              <th>Score</th>
+              <th>Users</th>
+              <th>Answered</th>
+              <th>Created</th>
             </tr>
           </thead>
-          <tbody>{/* Add your leaderboard data here */}</tbody>
+          <tbody>
+            {orderedUsers.map((u) => (
+              <tr key={u.id}>
+                <td>
+                  <div className="user-info">
+                    <img
+                      src={u.avatarURL}
+                      alt={`Avatar of ${u.name}`}
+                      className="user-avatar"
+                    />
+                    <span>{u.name}</span>
+                  </div>
+                </td>
+                <td>{u.answeredCount}</td>
+                <td>{u.createdCount}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
