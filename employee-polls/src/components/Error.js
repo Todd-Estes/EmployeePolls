ed43306b0ac643fc44e1bf6b357cb4d783af31ea
todd-estes <elestes@gmail.com>
2024-10-03 11:21:04 -0400

    refactor: Enhance 404 screen with styling

--- employee-polls/src/components/Error.js
+++ employee-polls/src/components/Error.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>Error 404</div>
-  )
-}
+    <div className="error-container">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      <Link to="/" className="home-link">
+        Go to Home
+      </Link>
+    </div>
+  );
+};
 
 export default Error;
