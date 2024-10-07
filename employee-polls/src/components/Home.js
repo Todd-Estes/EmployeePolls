4e2cf48f714efc962e74f47b1ac88eec294556a5
todd-estes <elestes@gmail.com>
2024-10-07 16:28:41 -0400

    feat: Implement question toggle

--- employee-polls/src/components/Home.js
+++ employee-polls/src/components/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import QuestionCard from './QuestionsContainer';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import QuestionsContainer from "./QuestionsContainer";
 
 const Home = () => {
-  const authedUser = useSelector(state => state.authedUser);
-  const loggedInUser = useSelector(state => state.users[authedUser]);
-  const questions = useSelector(state => state.questions);
+  const [showAnswered, setShowAnswered] = useState(false);
+  const authedUser = useSelector((state) => state.authedUser);
+  const loggedInUser = useSelector((state) => state.users[authedUser]);
+  const questions = useSelector((state) => state.questions);
 
   const answeredQuestionIds = Object.keys(loggedInUser.answers);
 
   const questionsByType = () => {
     return Object.values(questions).reduce(
       ([answered, unanswered], question) => {
-        (answeredQuestionIds.includes(question.id) ? answered : unanswered).push(
-          question
-        );
+        (answeredQuestionIds.includes(question.id)
+          ? answered
+          : unanswered
+        ).push(question);
         return [answered, unanswered];
       },
       [[], []]
@@ -23,12 +25,28 @@ const Home = () => {
 
   const [answeredQuestions, newQuestions] = questionsByType();
 
+  const toggleQuestions = () => {
+    setShowAnswered(!showAnswered);
+  };
+
   return (
     <div className="container">
       <div className="content">
         <h1>Home</h1>
-        <QuestionCard questions={answeredQuestions} cardTitle={'Answered Questions'}/>
-        <QuestionCard questions={newQuestions} cardTitle={'New Questions'}/>
+        <button onClick={toggleQuestions}>
+          {showAnswered ? "Show New Questions" : "Show Answered Questions"}
+        </button>
+        {showAnswered ? (
+          <QuestionsContainer
+            questions={answeredQuestions}
+            cardTitle={"Answered Questions"}
+          />
+        ) : (
+          <QuestionsContainer
+            questions={newQuestions}
+            cardTitle={"New Questions"}
+          />
+        )}
       </div>
     </div>
   );
