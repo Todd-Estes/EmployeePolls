1306f6df1767709946c3cd996eb864bb795fdf0b
todd-estes <elestes@gmail.com>
2024-10-07 01:39:57 -0400

    test: Add login test; includes snapshot and fireEvent()

---
+++ employee-polls/src/components/Login.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { render, waitFor, fireEvent, screen, cleanup } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../store/index";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllTimers();
+});
+
+test('renders login drop down', async () => {
+  render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  );
+
+  await waitFor(() => {
+    const state = store.getState();
+    expect(state.users).toBeTruthy();
+    expect(Object.keys(state.users).length).toBeGreaterThan(0);
+    expect(state.questions).toBeTruthy();
+    expect(Object.keys(state.questions).length).toBeGreaterThan(0);
+  }, { timeout: 5000 });
+
+  await waitFor(() => {
+    expect(screen.getByText(/Select a user.../i)).toBeInTheDocument();
+  });
+
+  const dropdown = screen.getByRole("combobox");
+  expect(dropdown).toBeInTheDocument();
+
+  expect(store.getState().authedUser).toBe(null);
+
+  const user = Object.values(store.getState().users)[0];
+  fireEvent.change(dropdown, { target: { value: user.id } });
+
+  await waitFor(() => {
+    expect(store.getState().authedUser).toBe(user.id);
+  });
+
+  expect(screen).toMatchSnapshot();
+})
