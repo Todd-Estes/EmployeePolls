96ca278026da546bcb2d910de74ea2cf9c88fa88
todd-estes <elestes@gmail.com>
2024-10-08 16:22:30 -0400

    style: Enhance New Question page

--- employee-polls/src/components/NewQuestion.js
+++ employee-polls/src/components/NewQuestion.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { generateCustomId } from '../utils/helpers';
-import { handleAddQuestion } from '../store/combinedActions';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleAddQuestion } from "../store/combinedActions";
 
 const NewQuestion = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const authedUser = useSelector((state) => state.authedUser);
 
-  const [optionOneInput, setOptionOneInput] = useState('');
-  const [optionTwoInput, setOptionTwoInput] = useState('');
+  const [optionOneInput, setOptionOneInput] = useState("");
+  const [optionTwoInput, setOptionTwoInput] = useState("");
 
   const updateOptionOneInput = (value) => {
     setOptionOneInput(value);
@@ -31,27 +30,34 @@ const NewQuestion = () => {
         optionTwoText: optionTwoInput,
       })
     );
-    navigate('/');
+    navigate("/");
   };
 
-
   return (
-    <div>
+    <div className="new-question-container">
       <h1>Would You Rather</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="new-question-form">
         <input
           type="text"
           placeholder="Option One"
           value={optionOneInput}
           onChange={(event) => updateOptionOneInput(event.target.value)}
+          className="new-question-input"
         />
         <input
           type="text"
           placeholder="Option Two"
           value={optionTwoInput}
           onChange={(event) => updateOptionTwoInput(event.target.value)}
+          className="new-question-input"
         />
-        <button disabled={inputsMissing} type="submit">Submit</button>
+        <button
+          disabled={inputsMissing}
+          type="submit"
+          className="new-question-submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
