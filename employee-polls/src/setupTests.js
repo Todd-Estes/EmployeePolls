11c891ce88003f4ce109fdf3de24418da916d87e
todd-estes <elestes@gmail.com>
2024-10-09 19:26:49 -0400

    fix: Upgrade "@testing-library/react to suppress test warnings

--- employee-polls/src/setupTests.js
+++ employee-polls/src/setupTests.js
@@ -5,22 +5,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
-// Suppress specific React warnings
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args) => {
-    if (
-      /Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`/.test(
-        args[0]
-      )
-    ) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-
-afterAll(() => {
-  console.error = originalError;
-});
